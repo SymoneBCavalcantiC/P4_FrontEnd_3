9dd6267bc1c0f7297455c32b8f4d2a4140f63054
SymoneBCavalcantiC <2022210220016@iesp.edu.br>
2024-03-06 05:34:23 -0300

    Resolução da Atividade 3 React

--- aula01/src/App.jsx
+++ aula01/src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Adicao from './components/Adicao'
+import Subtracao from './components/Subtracao'
+import Multiplicacao from './components/Multiplicacao'
+import Divisao from './components/Divisao'
+import PrecisoEstudar from './components/PrecisoEstudar'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello, World!!!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>Primeira atividade da disciplina 'Tecnologias para Frontend III', Prof.Kelson Almeida, UNIESP 2024.1</p>
-        <p>Aluna: Symone Cavalcanti</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Adicao num1={5} num2={4}/>
+      <Subtracao num1={5} num2={4} />
+      <Multiplicacao num1={5} num2={4} />
+      <Divisao num1={5} num2={4} />
+      <PrecisoEstudar nomeDaTecnologia={"REACT"} />
     </>
   )
 }
